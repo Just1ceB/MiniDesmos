605c09aa3dfc7e3375cc83d83daca1e8b5f925a2
Just1ceB <chpshonok@gmail.com>
2026-05-17 13:22:31 +0300

    wtf why ts breaks D:

--- Line.cs
+++ Line.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Prjcts
+namespace MiniDesmos
 {
     internal class Line
     {
--- Parabola.cs
+++ Parabola.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Prjcts
+namespace MiniDesmos
 {
     internal class Parabola
     {
